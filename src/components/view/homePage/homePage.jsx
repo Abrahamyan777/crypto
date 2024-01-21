9ce5354bd6f9b56c4841d1bb9f7362cfa171486b
abrahamyan77 <abrahamyan051996@mail.com>
2024-01-21 23:31:53 +0400

    add languges

--- src/components/view/homePage/homePage.jsx
+++ src/components/view/homePage/homePage.jsx
@@ -1,20 +1,25 @@
+import { useTranslation } from 'react-i18next';
 import Header from './header/header';
 import './homePage.scss'
 
 
 
+
 const HomePage = ({ active, setActive }) => {
+
+    const { t, i18n } = useTranslation();
+    
     return (
         <div className="homePageWrapper">
             <Header active={active} setActive={setActive} />
             <div className='left-section'>
-                <h6>Лучший проект</h6>
-                <h1>Крипто</h1>
-                <h2>Инвестиции</h2>
-                <p>Международный проект инвестиции в криптовалюту и <br />не только. Мы заработаем если вы заработаете!</p>
+                <h6>{t("homepage.Best project")}</h6>
+                <h1>{t("homepage.Crypto")}</h1>
+                <h2>{t("homepage.Investments")}</h2>
+                <p>{t("homepage.text1")} <br />{t("homepage.text2")}</p>
                 <div className='div-btn'>
-                    <button className='btn1'>  Присоединиться</button>
-                    <button className='btn2'>Подробнее</button>
+                    <button className='btn1'>  {t("homepage.Join")}</button>
+                    <button className='btn2'>{t("homepage.More details")}</button>
                 </div>
                 <div className='klients'>
                 
--- src/components/view/infoPage/infoPage.jsx
+++ src/components/view/infoPage/infoPage.jsx
@@ -2,55 +2,46 @@ import './infoPage.scss';
 import img1 from './../../../images/pngwing1.png';
 import img2 from './../../../images/pngwing2.png';
 import img3 from './../../../images/pngwing3.png';
+import { useTranslation } from 'react-i18next';
 
 
 const InfoPage = () => {
+
+    const { t, i18n } = useTranslation();
+
+
     return (
         <div className="infoPage-wrapper">
             <div className='infoPage-top'>
-                <h1 className='title'>Как “Invest Coin” работает? </h1>
-                <p className='text'>Invest Coin - это команда оптыных инвесторов,
-                    аналитиков и программистов. Мы работаем на площадках криптовалют
-                    (и не только) уже более 4 года и имеем конкурентоспособное портфолио.
-                    Все встречи, общения и договоренности
-                    с нашими партнерами (клиентами) проходят в прозрачной форме.
-                </p>
+                <h1 className='title'>{t("InfoPage.Coin")}</h1>
+                <p className='text'>{t("InfoPage.CoinText")} </p>
             </div>
             <div className='infoPage-bottom'>
                 <div className='finance finance1'>
                     <div className='img-div'>
                         <img src={img1} alt='pngwing1' className='pngwing1-img' />
                     </div>
-                    <h3 >Надежность</h3>
+                    <h3 >{t("InfoPage.Reliability")}</h3>
                     <p>
-                        Slate helps you see how
-                        many more days you need
-                        to work to reach your
-                        financial goal.
+                    {t("InfoPage.h3text")}
                     </p>
                 </div>
                 <div className='finance finance2'>
                     <div className='img-div'>
                         <img src={img2} alt='pngwing2' className='pngwing2-img' />
                     </div>
-                    <h3 >Опыт</h3>
+                    <h3 >{t("InfoPage.Experience")}</h3>
                     <p>
-                        Slate helps you see how
-                        many more days you need
-                        to work to reach your
-                        financial goal.
+                    {t("InfoPage.h3text2")}
                     </p>
                 </div>
                 <div className='finance finance3'>
                     <div className='img-div'>
                         <img src={img3} alt='pngwing3' className='pngwing3-img' />
                     </div>
-                    <h3 >Прозрачность</h3>
+                    <h3 >{t("InfoPage.Transparency")}</h3>
                     <p>
-                        Slate helps you see how
-                        many more days you need
-                        to work to reach your
-                        financial goal.
+                    {t("InfoPage.TransparencyText")}
                     </p>
                 </div>
             </div>
